072d18c8901e17c854fb7ff13b29e78a0431ba46
Dipanwita <susiejojo1@gmail.com>
2020-12-03 20:58:15 +0530

    Add graphHopper routing modes

--- client/src/components/RoutingMachine.js
+++ client/src/components/RoutingMachine.js
@@ -1,16 +1,24 @@
 import { MapLayer } from "react-leaflet";
 import L from "leaflet";
 import "leaflet-routing-machine";
+import "lrm-graphhopper";
 import { withLeaflet } from "react-leaflet";
 
 class Routing extends MapLayer {
   createLeafletElement() {
     const { map } = this.props;
     let leafletElement = L.Routing.control({
-      waypoints: [L.latLng(22.5259, 88.3702), L.latLng(17.3850, 78.4867)],
+      waypoints: [L.latLng(22.5259, 88.3702), 
+        L.latLng(17.3850, 78.4867)],
       fitSelectedRoutes: true,
       useZoomParameter: true,
-      draggableWaypoints: false
+      draggableWaypoints: false,
+      // geocoder: L.Control.geocoder(),
+      router: L.Routing.graphHopper(process.env.REACT_APP_GRAPHHOPPER_API_KEY,{
+        urlParameters:{
+          vehicle:'hike'
+        }
+      })
     }).addTo(map.leafletElement);
     leafletElement.hide();
     return leafletElement.getPlan();
