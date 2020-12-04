9f5c9c8a8bd4f74ebf5105aac1e141d49ed88553
Dipanwita <susiejojo1@gmail.com>
2020-12-05 04:42:53 +0530

    Call api endpoint while loading map

--- client/src/routes/Main.js
+++ client/src/routes/Main.js
@@ -3,6 +3,14 @@ import 'leaflet/dist/leaflet.css';
 import MapView from '../components/MapView';
 
 const Main = (props) => {
+    var user_input = props.location.state;
+    var origin = user_input.origin.lat+","+user_input.origin.lon;
+    var dest = user_input.dest.lat+","+user_input.dest.lon;
+    fetch("http://localhost:5000/api?origin="+origin+"&dest="+dest+"&mode=drive")
+            .then((res) => res.json())
+            .then((data) => {
+               console.log(data);
+        });
     return (
         <div>
             <MapView loc_data = {props.location.state}/>
