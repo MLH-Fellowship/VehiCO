e7b22a7da9821abd927685154329825f66ab0073
Chau Vu <chv004@ucsd.edu>
2020-12-05 01:54:16 -0800

    Sidebar with hardcoded values

--- client/src/routes/Main.js
+++ client/src/routes/Main.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import 'leaflet/dist/leaflet.css';
+import { makeStyles } from '@material-ui/core/styles';
+import { Grid } from '@material-ui/core';
 import MapView from '../components/MapView';
+import TripReport from '../components/TripReport';
+
+const useStyles = makeStyles({
+    root: {
+        marginRight: "2vw",
+        marginLeft: "2vw",
+    },
+    logo: {
+        fontSize: '4vw',
+        fontWeight: "normal",
+        color: '#04034E',
+        marginBottom: '2vh',
+        marginTop: '0.5vh'
+    },
+});
 
 const Main = (props) => {
+    const classes = useStyles();
     console.log(props.location.state);
     return (
-        <div>
-            <MapView />
+        <div className={classes.root}>
+            <Grid container justify="center" direction="row" alignItems="center" spacing={0} className={classes.grid}>
+            <Grid item xs={7}>
+                    <MapView />
+                </Grid>
+                <Grid item xs={4}>
+                    <h1 className={classes.logo}>VehiCO</h1>
+                    <TripReport />
+                </Grid>
+            </Grid>
         </div>
     )
 }
